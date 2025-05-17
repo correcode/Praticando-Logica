5af02249b86d6b96b452b3b94594bfdaa68d8093
Alisson Correa Alves <aliba.truta@gmail.com>
2025-05-16 21:23:46 -0300

    Dia 1 - Verificando numeros primos Array Simples
    Signed-off-by: Alisson Correa Alves <aliba.truta@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
-        int[] numeros = {1,3,5,7,9};
-        boolean temPar = false;
-
-        System.out.println("Numeros pares na lista: ");
-
-        for (int numero : numeros) {
-            if (numero % 2 == 0) {
-                System.out.println("Numero: " + numero);
-                temPar = true;
+        System.out.println("Numeros Primos: ");
+        for(int numero : numeros) {
+            if(ehPrimo(numero)){
+                System.out.println(numero + " ");
             }
         }
-        if (!temPar){
-            System.out.println("Nao ha numeros pares na lista.");
+    }
+    public static boolean ehPrimo(int n){
+        if(n <= 1) return false;
+
+        for(int i = 2; i < Math.sqrt(n); i++){
+            if(n % i == 0) return false;
         }
+        return true;
     }
 }
