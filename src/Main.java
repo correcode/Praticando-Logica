9deee64524e98791f5ca94f23d1e8fd9ad1df1f0
Alisson Correa Alves <aliba.truta@gmail.com>
2025-05-18 19:54:53 -0300

    Exercicio verificando se uma string e palindroma
    Signed-off-by: Alisson Correa Alves <aliba.truta@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    public static List<String> fizzBuzz(int n) {
-
-        List<String> result = new ArrayList<>();
-
-        for(int i = 1; i <= n; i++) {
-            if(i % 3 == 0 && i % 5 == 0) {
-                result.add("FizzBuzz");
-            }
-            else if(i % 3 == 0) {
-                result.add("Fizz");
-            }
-            else if(i % 5 == 0) {
-                result.add("Buzz");
-            }
-            else {
-                result.add(String.valueOf(i));
-            }
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        int n = 15;
-        List<String> resposta = fizzBuzz(n);
 
-        for(String s : resposta) {
-            System.out.println(s);
+        String palavra1 = "casa";
+        String palavra2 = "arara";
+
+        System.out.println(palavra1 + " e palindroma ? " + ehPalindromo(palavra1));
+        System.out.println(palavra2 + " e palindroma ? " + ehPalindromo(palavra2));
+
+    }
+    public static boolean ehPalindromo(String texto) {
+
+        int esquerdo = 0;
+        int direito = texto.length() -1;
+
+        while(esquerdo < direito) {
+            if(texto.charAt(esquerdo) != texto.charAt(direito)) {
+                return false;
+            }
+            esquerdo++;
+            direito--;
         }
+        return true;
     }
 }
